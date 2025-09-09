3d38235de0db129c7ab6d7e12dd7497c804d3f85
neven <nevenmitrovic4@gmail.com>
2025-09-09 17:32:47 +0200

    add user profile interface

--- types/index.ts
+++ types/index.ts
@@ -7,3 +7,12 @@ export interface IInput {
 	type: 'text' | 'number'
 	placeholder: string
 }
+export interface IUserProfile {
+	id: string
+	fullName: string
+	year: string
+	active: string
+	skillsList: string[]
+	interests: string[]
+	whatsApp: string
+}
