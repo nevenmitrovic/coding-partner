130d3cbc892bfc963a34c56eea5489d3cc5de465
neven <nevenmitrovic4@gmail.com>
2025-09-12 16:33:52 +0200

    update form page with next form component

--- app/(content)/teams/@form/page.tsx
+++ app/(content)/teams/@form/page.tsx
@@ -1,15 +1,17 @@
+import Form from 'next/form'
+
 import FormHeader from '@/components/content/teams/form/form-header/form-header'
 import FormContent from '@/components/content/teams/form/form-content/form-content'
 import styles from './page.module.css'
 import FormSubmit from '@/components/content/teams/form/form-submit/form-submit'
 import { createTeam } from '@/actions/teams'
 
-export default function Form() {
+export default function FormPage() {
 	return (
-		<form className={styles.form} action={createTeam}>
+		<Form className={styles.form} action={createTeam}>
 			<FormHeader />
 			<FormContent />
 			<FormSubmit />
-		</form>
+		</Form>
 	)
 }
