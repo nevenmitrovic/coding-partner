6bd012d146daccf1177b6b6a58d4c08b59a90f98
neven <nevenmitrovic4@gmail.com>
2025-09-22 15:30:42 +0200

    update navigation with logged status and signout button

--- components/common/header/navigation/navigation.tsx
+++ components/common/header/navigation/navigation.tsx
@@ -1,14 +1,33 @@
+'use client'
+
+import { authClient } from '@/auth/auth-client'
 import NavLink from '@/components/common/header/common/nav-link/nav-link'
-import styles from './navigation.module.css'
 import { NAVIGATION } from '@/constants'
 
+import styles from './navigation.module.css'
+
 export default function Navigation() {
+	const { data, error } = authClient.useSession()
+
+	const handleSignOut = async () => {
+		await authClient.signOut()
+	}
+
 	return (
 		<nav className={styles.navigation}>
 			<ul>
-				{NAVIGATION.map((item) => (
-					<NavLink key={item.label} {...item} />
-				))}
+				{NAVIGATION.map((item) => {
+					if (item.label === '[Login]' && data) {
+						return (
+							<li key={item.label}>
+								<button className={styles.signOut} onClick={handleSignOut}>
+									Logout
+								</button>
+							</li>
+						)
+					}
+					return <NavLink key={item.label} {...item} />
+				})}
 			</ul>
 		</nav>
 	)
